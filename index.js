9a91c32ec5096382f7e021bb9ea796c977cb8a0a
Zakiya-Abass <zakiyaabass@icloud.com>
2023-01-11 18:45:30 +0300

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = 1;
+Math.random();
+
+let num3 = 16;
+let num4 = 4;
+let mod = num3 / num4;
+
+let max = (1, 2, 20);
+max();
